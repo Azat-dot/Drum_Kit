ae1f75855a83893c11904b8f8458a796cedea928
azat <azatsafaryan@mail.ru>
2022-06-15 19:05:34 +0500

    change color

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+
+
+var audio = new Audio('tom-1.mp3')
 for ( i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("hey")
+        // var audio = new Audio("./sounds/tom-1.mp3")
+        // audio.play();
+        this.style.color = "white"
     })
 }
+
+ //// calculator in console
+// function add( num1, num2) {
+//     return num1 + num2; }
+// function subtrackt(num1, num2) {
+//     return num2 - num1;}
+// function multyplay(num1, num2) {
+//     return num1 * num2}
+// function divide(num1, num2) {
+//     return num2 / num1}
+// function calculator( num1, num2, operator) { return operator(num1, num2)};
