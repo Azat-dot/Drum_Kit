b8bee7bbe48defed475afb38b0a6abacb0255ec5
azat <azatsafaryan@mail.ru>
2022-06-16 13:59:38 +0500

    All drams work

--- index.js
+++ index.js
@@ -3,9 +3,43 @@
 var audio = new Audio('tom-1.mp3')
 for ( i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        // var audio = new Audio("./sounds/tom-1.mp3")
-        // audio.play();
-        this.style.color = "white"
+        
+        this.style.color = "blue"
+var buttonInnerHTML = this.innerHTML;        
+ 
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3")
+                tom1.play();
+            break;
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3")
+                tom2.play();
+            break;
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+            break;
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3")
+                tom4.play();
+            break;
+            case "j":
+                var snare = new Audio("sounds/snare.mp3")
+                snare.play();
+            break;
+            case "k":
+                var crash = new Audio("sounds/crash.mp3")
+                crash.play();
+            break;
+            case "l":
+                var kick = new Audio("sounds/kick-bass.mp3")
+                kick.play();
+            break;
+        
+            default: console.log(buttonInnerHTML);
+               
+        }
     })
 }
 
@@ -19,3 +53,4 @@ for ( i = 0; i < document.querySelectorAll(".drum").length; i++) {
 // function divide(num1, num2) {
 //     return num2 / num1}
 // function calculator( num1, num2, operator) { return operator(num1, num2)};
+
